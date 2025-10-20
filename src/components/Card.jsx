15b2998d64e55371d5094d452852b11b22db7536
emilly-ksalmeida <emilly.eksa@gmail.com>
2025-10-20 14:13:55 -0300

    Adiciona funcionalidade para selecionar e compor itens de um pedido.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,43 @@
-import React from "react";
-import ImgProduto from "../assets/pratos/burguer-picanha.jpeg";
+import { useForm } from "react-hook-form";
 
-const Card = ({ itemCategoria }) => {
-  const { nome, preco, imagem, descricao } = itemCategoria;
+const Card = (props) => {
+  const { register, handleSubmit, reset } = useForm();
+
+  function adicionar(qtdeItem) {
+    let numQtdeItem = Number(qtdeItem.qtde);
+    const novaCompra = {
+      produto: props.titulo,
+      quantidade: numQtdeItem,
+      precoUnitario: props.preco,
+    };
+    //Guardar a informação do pedido
+    reset();
+  }
 
   return (
-    <div className="card">
-      <div className="card__textos">
-        <h2 className="card__titulo">{nome}</h2>
-
-        <p>{descricao}</p>
-
-        <p className="card__preco">{preco}</p>
-      </div>
-
-      <div className="card__img">
-        <img src={imagem} alt="Imagem do Produto" />
+    <div>
+      <div className="card">
+        <div className="card__textos">
+          <h2 className="card__titulo">{props.titulo}</h2>
+          <p>{props.descricao}</p>
+          <p className="card__preco">{props.preco}</p>
+        </div>
+        <div className="card__img">
+          <img src={props.imagem} alt={props.descricao} />
+        </div>
+        <div>
+          <form onSubmit={handleSubmit(adicionar)}>
+            <label>Quantidade: </label>
+            <input
+              {...register("qtde")}
+              type="number"
+              min="0"
+              max="10"
+              step="1"
+            />
+            <button type="submit">Adicionar</button>
+          </form>
+        </div>
       </div>
     </div>
   );
