17841220b91db9befebc64e7ebfb3d73fd74c966
emilly-ksalmeida <emilly.eksa@gmail.com>
2025-10-20 14:24:57 -0300

    Aplica a Context API para armazenar e gerenciar o estado do pedido (itens selecionados, quantidade etc.), permitindo que ele seja acessado e alterado em todas as páginas da aplicação.

--- src/main.jsx
+++ src/main.jsx
@@ -3,20 +3,23 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
 import Pedido from "./pages/Pedido.jsx";
+import PedidosContext from "./context/PedidosContext.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />
+    element: <App />,
   },
   {
-    path:"/pedido",
-    element: <Pedido />
-  }
+    path: "/pedido",
+    element: <Pedido />,
+  },
 ]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <PedidosContext>
+      <RouterProvider router={router} />
+    </PedidosContext>
   </StrictMode>
 );
