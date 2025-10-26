18ed4d92ad77edbec0bfb66187e87227b3a7a2c9
emilly-ksalmeida <emilly.eksa@gmail.com>
2025-10-26 15:26:24 -0300

    Inicia a migração da estilização de CSS puro para Tailwindcss.

--- src/components/Navegacao.jsx
+++ src/components/Navegacao.jsx
@@ -1,15 +1,15 @@
 const Navegacao = ({ setCategoriaSelecionada, categoriaSelecionada }) => {
   const categoriasMenu = ["Pratos Principais", "Sobremesas", "Bebidas"];
   
-
   return (
-    <div className="categorias">
+    <div className="rounded-md bg-nav flex flex-row gap-8 py-4 px-4 items-center justify-center">
       {categoriasMenu.map((categoria, index) => (
         <p
           onClick={() => setCategoriaSelecionada(index)}
           key={index}
-          className={`categoria${
-            index === categoriaSelecionada ? " categoria--selecionada" : ""
+          className={`w-1/3 rounded-md text-center
+            p-2 text-white cursor-pointer ${
+            index === categoriaSelecionada ? "bg-category-selec transition-colors duration-200 ease-in-out" : ""
           }`}
         >
           {categoria}
